e4491db790c9da810fa65079286c997c8ffea704
Ross Chater <rdchater@gmail.com>
2024-05-12 14:10:24 +0100

    crawl/job/repository/mem + tests

--- api/crawl/job/repository/mem/mem.go
+++ api/crawl/job/repository/mem/mem.go
@@ -1,1 +1,45 @@
 package mem
+
+import "crawlquery/api/domain"
+
+type Repository struct {
+	jobs map[string]*domain.CrawlJob
+}
+
+func NewRepository() *Repository {
+	return &Repository{
+		jobs: make(map[string]*domain.CrawlJob),
+	}
+}
+
+func (r *Repository) Create(j *domain.CrawlJob) error {
+	r.jobs[j.ID] = j
+	return nil
+}
+
+func (r *Repository) Get(id string) (*domain.CrawlJob, error) {
+	job, ok := r.jobs[id]
+	if !ok {
+		return nil, nil
+	}
+
+	return job, nil
+}
+
+func (r *Repository) First() (*domain.CrawlJob, error) {
+
+	var earliest *domain.CrawlJob
+	for _, job := range r.jobs {
+
+		if earliest == nil || job.CreatedAt.Before(earliest.CreatedAt) {
+			earliest = job
+		}
+	}
+
+	return earliest, nil
+}
+
+func (r *Repository) Delete(id string) error {
+	delete(r.jobs, id)
+	return nil
+}
---
+++ api/crawl/job/repository/mem/mem_test.go
@@ -0,0 +1,119 @@
+package mem
+
+import (
+	"crawlquery/api/domain"
+	"testing"
+	"time"
+)
+
+func TestCreate(t *testing.T) {
+	repo := NewRepository()
+
+	job := &domain.CrawlJob{
+		ID:  "job1",
+		URL: "http://example.com",
+	}
+
+	err := repo.Create(job)
+
+	if err != nil {
+		t.Fatalf("Error creating job: %v", err)
+	}
+
+	if repo.jobs[job.ID].ID != job.ID {
+		t.Errorf("Expected ID to be %s, got %s", job.ID, repo.jobs[job.ID].ID)
+	}
+
+	if repo.jobs[job.ID].URL != job.URL {
+		t.Errorf("Expected URL to be %s, got %s", job.URL, repo.jobs[job.ID].URL)
+	}
+
+	if repo.jobs[job.ID].CreatedAt.Sub(job.CreatedAt) > time.Second || job.CreatedAt.Sub(repo.jobs[job.ID].CreatedAt) > time.Second {
+		t.Errorf("Expected CreatedAt to be within one second of %v, got %v", job.CreatedAt, repo.jobs[job.ID].CreatedAt)
+	}
+}
+
+func TestGet(t *testing.T) {
+	repo := NewRepository()
+
+	job := &domain.CrawlJob{
+		ID:  "job1",
+		URL: "http://example.com",
+	}
+
+	repo.jobs[job.ID] = job
+
+	got, err := repo.Get(job.ID)
+
+	if err != nil {
+		t.Fatalf("Error getting job: %v", err)
+	}
+
+	if got.ID != job.ID {
+		t.Errorf("Expected ID to be %s, got %s", job.ID, got.ID)
+	}
+
+	if got.URL != job.URL {
+		t.Errorf("Expected URL to be %s, got %s", job.URL, got.URL)
+	}
+
+	if got.CreatedAt.Sub(job.CreatedAt) > time.Second || job.CreatedAt.Sub(got.CreatedAt) > time.Second {
+		t.Errorf("Expected CreatedAt to be within one second of %v, got %v", job.CreatedAt, got.CreatedAt)
+	}
+}
+
+func TestFirst(t *testing.T) {
+	repo := NewRepository()
+
+	job1 := &domain.CrawlJob{
+		ID:  "job1",
+		URL: "http://example.com",
+	}
+
+	job2 := &domain.CrawlJob{
+		ID:  "job2",
+		URL: "http://example.com",
+	}
+
+	repo.jobs[job1.ID] = job1
+	repo.jobs[job2.ID] = job2
+
+	first, err := repo.First()
+
+	if err != nil {
+		t.Fatalf("Error getting first job: %v", err)
+	}
+
+	if first.ID != job1.ID {
+		t.Errorf("Expected ID to be %s, got %s", job1.ID, first.ID)
+	}
+
+	if first.URL != job1.URL {
+		t.Errorf("Expected URL to be %s, got %s", job1.URL, first.URL)
+	}
+
+	if first.CreatedAt.Sub(job1.CreatedAt) > time.Second || job1.CreatedAt.Sub(first.CreatedAt) > time.Second {
+		t.Errorf("Expected CreatedAt to be within one second of %v, got %v", job1.CreatedAt, first.CreatedAt)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	repo := NewRepository()
+
+	job := &domain.CrawlJob{
+		ID:  "job1",
+		URL: "http://example.com",
+	}
+
+	repo.jobs[job.ID] = job
+
+	err := repo.Delete(job.ID)
+
+	if err != nil {
+		t.Fatalf("Error deleting job: %v", err)
+	}
+
+	if _, ok := repo.jobs[job.ID]; ok {
+		t.Errorf("Expected job to be deleted, got %v", repo.jobs[job.ID])
+	}
+}
